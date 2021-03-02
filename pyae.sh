e2ebae893168fbae634b605086cbc6def34ee184
bgmpyaesonehmoo290G <79253197+bgmpyaesonehmoo290G@users.noreply.github.com>
2021-03-02 14:40:09 +0630

    Update pyae.sh

--- pyae.sh
+++ pyae.sh
@@ -321,7 +321,7 @@ echo -e $okegreen"   ====================== Tool Lest =======================   
                 read -p "pess any key to return ..."
                 clear
                 ;;
-            q)  echo -e $yellow " Thanks For Using My Tools Good Bye  {Ha3MrX}  !!"
+            q)  echo -e $yellow " Thanks For Using My Tools Good Bye  Min Lol😼🖕  !!"
                 echo
                 exit 0 
                 ;;
