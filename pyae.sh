b556621e2278925ca5bfc821e61da4b5170919f8
bgmpyaesonehmoo290G <79253197+bgmpyaesonehmoo290G@users.noreply.github.com>
2021-03-12 12:48:05 +0630

    Update pyae.sh

--- pyae.sh
+++ pyae.sh
@@ -22,8 +22,6 @@ yellow='\e[1;33m'
 blue='\e[1;34m'
 echo ""
 sleep 2
-# Check root
-[[ `id -u` -eq 0 ]] > /dev/null 2>&1 || { echo  $red "You must be root to run the script"; echo ; exit 1; }
 
 # detect ctrl+c exiting
 trap ctrl_c INT
